3d9c76472a0ec271578b04ebc4f9dd51ce06ab8b
mahel.brossier <mahel@langskip.net>
2024-07-30 15:09:06 +0200

    Initial Commit

--- tests/tp.py
+++ tests/tp.py
@@ -2,104 +2,59 @@
 
 
 def nombre_entier(n: int) -> str:
-	return f"{'S'*n}0"
+    return 'S'*n + '0'
 
 
 def S(n: str) -> str:
-	return f"S{n}"
-	
+    return 'S' + n
 
 
 def addition(a: str, b: str) -> str:
-	
-	""" 
-	a = "" if a == "0" else a[:-1]
-	b = "" if b == "0" else b[:-1]
-
-	if a == b == "0":
-		return "0" 
-	"""
-
-	if a == "0":
-		return b
-
-	return S(addition(a[1:], b))
-   
+    a = nombre_entier(a) if type(a) == int else a
+    b = nombre_entier(b) if type(b) == int else b
+    return 'S' * (a.count('S') + b.count('S')) + '0'
 
 
-def multiplication(n: str, x: str) -> str:
-
-	if n == "0":
-		return "0"
-
-	return  addition(multiplication(n[1:], x), x)
+def multiplication(a: str, b: str) -> str:
+    a = nombre_entier(a) if type(a) == int else a
+    b = nombre_entier(b) if type(b) == int else b
+    return 'S' * (a.count('S') * b.count('S')) + '0'
 
 
 def facto_ite(n: int) -> int:
-	accum = 1
-	for i in range(1, n+1):
-		accum *= i
-	return accum
+    r = 1
+    for i in range(n):
+        r *= i+1
+    return r
 
 def facto_rec(n: int) -> int:
-	if n == 0:
-		return 1
-	return n * facto_rec(n - 1)
+    if n == 0:
+        return 1
+    return facto_rec(n-1) * n
 
 
 def fibo_rec(n: int) -> int:
-	if n == 0:
-		return 0
-	if n == 1:
-		return 1
-	
-	return fibo_rec(n-1) + fibo_rec(n-2)
+    if n == 1:
+        return 1
+    if n == 0:
+        return 0
+    return fibo_rec(n-1) + fibo_rec(n-2)
 
 
 def fibo_ite(n: int) -> int:
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
 
-	if n == 0:
-		return 0
-	if n == 1:
-		return 1
-	else:
-		a = 0
-		b = 1
-		for i in range(n-1):
-			tmp = a + b
-			a = b
-			b = tmp
-		return b
-			
 
 def golden_phi(n: int) -> int:
-	#https://fr.wikipedia.org/wiki/Nombre_d%27or#Suite_de_Fibonacci
-	if n == 0:
-		return 1
-	return 1 + 1/golden_phi(n-1)
+    return (1 + sqrt5(n)) / 2
 
 
 def sqrt5(n: int) -> int:
-	return n ** (1/5)
+    return n ** (1/5)
 
 
 def pow(a: float, n: int) -> float:
-	if n == 0:
-		return 1
-	return a * pow(a, n-1)
-
-
-def pow2(a: float, n: int) -> float:
-	result = 1
-	for _ in range(n):
-		result *= a
-	return result 
-
-
-def pow3(a: float, n: int) -> float:
-	if n == 0:
-		return 1
-	if n % 2 == 0:
-		return pow3(a * a, n // 2)
-	else:
-		return a * pow3(a, n-1)
+    return a**n
